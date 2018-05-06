3f0525cb0c47c8397df539240a4b244c0581d095
CiinS Dim <dimc@os2.engr.oregonstate.edu>
2018-05-06 14:49:46 -0700

    Completed data collection settings functionality for arm raises (Except start)

---
+++ src/KinectPT/KinectPT/KinectPT/Pages/DataSettingsPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+
+namespace KinectPT
+{
+    /// <summary>
+    /// Interaction logic for DataSettingsPage.xaml
+    /// </summary>
+    public partial class DataSettingsPage : Page
+    {
+        public DataSettingsPage()
+        {
+            InitializeComponent();
+
+            exerciseStart.Text = Application.Current.Properties["beginAtExerciseStart"].ToString();
+        }
+
+        private void Click_Back(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+
+        void durationSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string selected = durationBox.SelectedValue.ToString();
+            Application.Current.Properties["durationUnit"] = selected.Substring(38, 7);
+            
+        }
+
+        
+    }
+
+    public class EnumMatchToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            string checkValue = value.ToString();
+            string targetValue = parameter.ToString();
+            return checkValue.Equals(targetValue,
+                     StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture)
+        {
+            if (value == null || parameter == null)
+                return null;
+
+            bool useValue = (bool)value;
+            string targetValue = parameter.ToString();
+            if (useValue)
+                return Enum.Parse(targetType, targetValue);
+
+            return null;
+        }
+    }
+}
---
+++ src/KinectPT/KinectPT/KinectPT/Pages/Home.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KinectPT
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+        private void Click_Exercises(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ExerciseOptions());
+
+        }
+
+        private void Click_Reports(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new ReportsPage());
+
+        }
+
+        private void Click_Data(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new DataSettingsPage());
+
+        }
+    }
+}
